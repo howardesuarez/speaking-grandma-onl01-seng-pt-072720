f1ed810345b14b5d028a833119418679086b91c8
howard suarez <howardesuarez@gmail.com>
2020-08-04 06:44:32 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -12,6 +12,10 @@
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 
-def speak_to_grandma
+phrase = "hi grandma."
+
+def speak_to_grandma(phrase)
+  
+  
   
 end
