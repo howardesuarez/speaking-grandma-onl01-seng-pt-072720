d8d012d8d738811e1ed12a65c97b4625f5a2993a
howard suarez <howardesuarez@gmail.com>
2020-08-04 06:48:34 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,19 +15,3 @@
 phrase = "I LOVE YOU GRANDMA!"
 
 def speak_to_grandma(phrase)
-  if phrase == "I LOVE YOU GRANDMA!"
-      return "I LOVE YOU TOO PUMPKIN!"
-    # If you shout, she can hear you (or at least she thinks so)
-    # and yells back
-    elsif phrase == phrase.downcase
-  		return "HUH?! SPEAK UP, SONNY!"
-    elsif phrase == phrase.downcase
-      return "HUH?! SPEAK UP, SONNY!"
-    elsif phrase == phrase.upcase
-      return "NO, NOT SINCE 1938!"
-    # NO, NOT SINCE 1938!
-    else
-  		return "NO, NOT SINCE 1938!"
-  
-  
-end
