741c6148540581e9099f18c68a0d583540aaa47e
howard suarez <howardesuarez@gmail.com>
2020-08-04 06:59:44 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -20,7 +20,7 @@ def speak_to_grandma(phrase)
     return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase.upcase
     return "NO, NOT SINCE 1938!"
-  else phrase.lowercase
+  else phrase
     return "HUH?! SPEAK UP, SONNY!"
   end
 end
