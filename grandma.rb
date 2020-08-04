43da5ee49eee0f03598d7402d564867bb7aade4a
howard suarez <howardesuarez@gmail.com>
2020-08-04 07:08:37 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -6,7 +6,10 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
   elsif phrase.upcase
-  puts 
+  puts "NO, NOT SINCE 1938!"
+else puts "HUH?! SPEAK UP, SONNY!"
+end
+
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
