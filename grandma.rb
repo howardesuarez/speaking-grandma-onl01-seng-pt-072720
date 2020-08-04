c6ddf7e0bc80474d4ffad42ada38fc7c898555aa
howard suarez <howardesuarez@gmail.com>
2020-08-04 07:03:25 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,9 @@
 # Write a speak_to_grandma method.
+phrase = "i love you grandma!"
 
+def speak_to_grandma(phrase)
+  
+  
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
